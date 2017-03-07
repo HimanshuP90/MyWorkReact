efacf410450e27a3cd359ee0b75148f53fd380f2
HimanshuP90 <himanshup895@gmail.com>
2017-03-07 13:02:17 +0530

    Initial commit 2

---
+++ webpack.config.dev.js
@@ -0,0 +1,23 @@
+import path from 'path'
+
+export default {
+  entry: path.join(__dirname, '/client/index.js'),
+  output: {
+  path: '/'
+},
+module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: [
+          path.join(__dirname, 'client')
+        ],
+        loaders: [ 'babel' ]
+      }
+    ]
+  },
+
+resolve: {
+  extentions: ['', '.js']
+}
+}
